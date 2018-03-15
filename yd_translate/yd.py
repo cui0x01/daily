4b95a9b3f72c563eea0d3676f45f8de7aac9ef99
cui0x01 <1067990798@qq.com>
2018-03-15 16:33:24 +0800

    yd_translate

---
+++ yd_translate/yd.py
@@ -0,0 +1,41 @@
+﻿import urllib.request
+import urllib.parse  #将data解析为url格式
+import json
+
+while True:
+    content = input("请输入需要翻译的内容(输入“q!”退出程序):")
+    if content == '':
+        print('请输入需要翻译的内容:')
+        continue
+    if content == 'q!':
+        break
+
+    url = 'http://fanyi.youdao.com/translate?smartresult=dict&smartresult=rule&smartresult=ugc&sessionFrom=https://www.baidu.com/link'
+
+    head = {}
+    head['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'
+
+
+    data = {}
+    data['type'] = 'AUTO'
+    data['i'] = content
+    data['doctype'] = 'json'
+    data['xmlVersion'] = '1.8'
+    data['keyfrom'] = 'fanyi.web'
+    data['ue'] = 'UTF-8'
+    data['action'] = 'FY_BY_CLICKBUTTON'
+    data['typoResult'] = 'true'
+    data = urllib.parse.urlencode(data).encode('utf-8')
+
+    req = urllib.request.Request(url,data,head)
+    response = urllib.request.urlopen(req)
+    html = response.read().decode('utf-8')
+
+    json.loads(html)
+    target = json.loads(html)
+    target = target['translateResult'][0][0]['tgt']
+    print(target)
+    
+
+
+
