972defdde46253ed3bbdbb953595f6e8b6b6b242
cui0x01 <1067990798@qq.com>
2018-03-20 22:02:22 +0800

    add function

--- bd_info/bd_info.py
+++ bd_info/bd_info.py
@@ -1,14 +1,17 @@
-#!/usr/bin/python3
-
-import socket,time
+'''
+@time:3/18/18 2:40 PM
+@Author:cui0x01
+'''
+import socket,time,paramiko
 client=socket.socket()
 
 global logdate
 logdate = time.strftime('%Y%m%d%H%M%S',time.localtime())
 
+filename=logdate+".txt"
 def w_log(file_name,data):
     with open(file_name+'.txt', 'a+') as f:
-        f.write(data.decode())
+        f.write(data)
 
 def telnet():
     client.connect(("10.8.12.80", 343))  #获取刚链接上的信息
@@ -35,7 +38,8 @@ def exec_cmd():
         time.sleep(0.3)
         data4 = client.recv(1024)
         #print(data4.decode())
-        w_log(logdate,data4)
+        w_log(logdate,data4.decode())
+    print("==>exec show cpu")
 
     client.send(b"show board all-all \n")   #show board all-all
     i=0
@@ -44,11 +48,12 @@ def exec_cmd():
         time.sleep(0.2)
         #print(data5.decode())
         i=i+1
-        w_log(logdate,data5)
+        w_log(logdate,data5.decode())
     # if len(client.recv(1024))>=0:
     #     time.sleep(0.3)
     #     data10 = client.recv(1024)
     #     print(data10.decode())
+    print("==>exec show board")
 
     client.send(b"show switchover\n")
     i = 0
@@ -57,11 +62,8 @@ def exec_cmd():
         time.sleep(0.2)
         #print(data6.decode())
         i = i + 1
-    # if len(client.recv(1024))>=0:
-    #     time.sleep(0.3)
-    #     data6 = client.recv(1024)
-        w_log(logdate,data6)
-
+        w_log(logdate,data6.decode())
+    print("==>exec show switchover")
 
     client.send(b"show alarm all \n")
     i = 0
@@ -70,13 +72,39 @@ def exec_cmd():
         time.sleep(0.2)
         #print(data7.decode())
         i = i + 1
-        w_log(logdate,data7)
+        w_log(logdate,data7.decode())
+    print("==>exec show show alarm")
+    client.close()
+    #print(client) 检查socket是否关闭
+
+#so add/delete
+def sub_opreat():
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect("10.8.12.80", 22, "admin", "admin")
+    command = {
+        1: "cresub n:0998082200 anw:1280genew l3addr:200 analog",
+        2: "cresub n:0998082201 anw:1280genew l3addr:201 analog",
+        3: "delsub n:0998082200",
+        4: "delsub n:0998082201",
+    }
+
+    for k in command:
+        print(command[k])
+        w_log(logdate,command[k]+"\n")
+        stdin, stdout, stderr = ssh.exec_command(command[k])
+        time.sleep(0.3)
+        #print(stdout.read().decode("gbk"))
+        res=stdout.read()
+        w_log(logdate,res.decode("gbk"))
+    ssh.close()
+
 
 if __name__ == "__main__":
     telnet()
+    print("==>telnet success")
     exec_cmd()
+    sub_opreat()
 
-
-client.close()
-print("_____check_success_____")
+print("filename:%s download success"%filename)
 
